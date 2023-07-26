0746f6258cc9e95c9ed7dc06e7d28421eab2dc60
RubelAzad <rubelazad123@gmail.com>
2023-07-26 08:28:11 +0600

    order status mail

--- resources/views/emails/order_status_changed.blade.php
+++ resources/views/emails/order_status_changed.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h3>Customer Name: {{$orderStatus['customer_name']}}</h3>
-    <p>Invoice ID: IFAD-{{$orderStatus['order_id'] }}</p>
-    <p>The order status has been changed to: {{ $orderStatus['status'] }}</p>
+    <p>Order ID: IFAD-{{$orderStatus['order_id'] }}</p>
+    <p>Order status: {{ $orderStatus['status'] }}</p>
 </body>
 </html>
