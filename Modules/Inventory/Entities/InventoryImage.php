ef08c84b14f6c09a7677fef7ebc8ecd47c8e5590
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-07-26 11:17:45 +0600

    Inventory Multi Image CRUD

--- Modules/Inventory/Entities/InventoryImage.php
+++ Modules/Inventory/Entities/InventoryImage.php
@@ -5,12 +5,13 @@ namespace Modules\Inventory\Entities;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Inventory\Entities\Inventory;
+use Modules\Base\Entities\BaseModel;
 
-class InventoryImage extends Model
+class InventoryImage extends BaseModel
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = ['inventory_id', 'image', 'status', 'created_at', 'updated_at'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
@@ -19,6 +20,55 @@ class InventoryImage extends Model
     {
         return $this->belongsTo(Inventory::class, 'inventory_id', 'id');
     }
+    private function get_datatable_query()
+    {
+        if(permission('pimage-bulk-delete')){
+            $this->column_order = [null,'id', 'inventory_id', 'image', 'status',null];
+        }else{
+            $this->column_order = [null,'id', 'inventory_id', 'image', 'status',null];
+        }
+
+        $query = self::with('inventory');
+
+
+        /*****************
+         * *Search Data **
+         ******************/
+        if (!empty($this->name)) {
+            $query->where('name', 'like', '%' . $this->name . '%');
+        }
+        if (!empty($this->inventory_id)) {
+            $query->where('inventory_id', $this->inventory_id);
+        }
+
+        if (isset($this->orderValue) && isset($this->dirValue)) {
+            $query->orderBy($this->column_order[$this->orderValue], $this->dirValue);
+        } else if (isset($this->order)) {
+            $query->orderBy(key($this->order), $this->order[key($this->order)]);
+        }
+        return $query;
+    }
+
+    public function getDatatableList()
+    {
+        $query = $this->get_datatable_query();
+        if ($this->lengthVlaue != -1) {
+            $query->offset($this->startVlaue)->limit($this->lengthVlaue);
+        }
+        return $query->get();
+    }
+
+    public function count_filtered()
+    {
+        $query = $this->get_datatable_query();
+        return $query->get()->count();
+    }
+
+    public function count_all()
+    {
+        return self::toBase()->get()->count();
+    }
+
 
     protected static function newFactory()
     {
